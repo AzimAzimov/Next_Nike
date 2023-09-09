b365ed35a544a72e2ac5913369f8b823199a6a60
Azim Azimov <mrazim0990@gmail.com>
2023-09-09 11:36:09 +0300

    master

---
+++ components/FavoritesItem.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { RiDeleteBin6Line } from "react-icons/ri"
+import Image from "next/image";
+import Link from 'next/link';
+import { useDispatch } from 'react-redux';
+
+import { removeFavorite } from '@/store/favoriteSlice';
+
+const FavoritesItem = ({ data }) => {
+  const prod = data?.attributes
+  const dispatch = useDispatch()
+  const handleRemoveFavorite = () => {
+    dispatch(removeFavorite({id: data.id}))
+  }
+
+  return (
+    <div className='flex py-5 gap-3 md:gap-5 border-b'>
+      <div className="shrink-0 aspect-square w-[50px] md:w-[120px]">
+        <Image
+          width={120}
+          height={120}
+          src={prod?.thumbnail?.data?.attributes.url}
+          alt={prod.name}
+        />
+      </div>
+      <div className='w-full flex flex-col'>
+        <div className='flex flex-col md:flex-row justify-between'>
+          <div className="text-lg md:text-2xl font-semibold text-black/[0.8]">
+            {prod.name}
+          </div>
+          <div className="text-sm md:text-md font-medium text-black/[0.5] block md:hidden">
+            {prod.subtitle}
+          </div>
+          <div className="text-sm md:text-md font-bold text-black/[0.5]">
+            MRP : $ {prod.price}
+          </div>
+        </div>
+        <div className="text-md font-medium text-black/[0.5] hidden md:block">
+          {prod.subtitle}
+        </div>
+
+        <div className='flex items-center justify-between mt-4'>
+          <div className="flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md">
+            <Link href={`/product/${prod.slug}`}
+              className="text-[12px] w-full py-[8px] px-[10px] rounded-full bg-black text-white transition-transform active:scale-95 hover:opacity-75"
+            >
+              Learn More
+            </Link>
+          </div>
+          <RiDeleteBin6Line
+            onClick={handleRemoveFavorite}
+            className='cursor-pointer text-black/[0.5] hover:text-black text-[16px] md:text-[20px]'
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FavoritesItem
---
+++ pages/index.js
@@ -0,0 +1,44 @@
+
+import React from 'react'
+
+import HerroBanner from '@/components/HerroBanner'
+import ProductCard from '@/components/ProductCard';
+import Wrapper from '@/components/Wrapper'
+import { fetchDataFromApi } from '@/utils/api';
+
+export default function Home ({ products }) {
+  return (
+    <main>
+      <HerroBanner />
+      <Wrapper>
+        {/* heading and paragraph start */}
+        <div className='max-w-[800px] mx-auto text-center my-[50px] mr:my-[80px]'>
+          <div className='leading-tight text-[28px] mb-5 font-semibold md:text-[34px]'>
+            Cushioning for You Miles
+          </div>
+          <div className='text-md md:text-xl'>
+            A lightweight Nike ZoomX midsole is combined with increased stack heights to help provide cushioning during extended stretches of running
+          </div>
+        </div>
+        {/* heading and paragraph end */}
+
+        {/*product grid start */}
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0'>
+          {products?.data?.map((product) => {
+            return (
+              <ProductCard key={product?.id} data={product} />
+            )
+          })}
+        </div>
+        {/*product grid end */}
+      </Wrapper>
+    </main>
+  )
+}
+
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*")
+  return {
+    props: {products}
+  }
+}
